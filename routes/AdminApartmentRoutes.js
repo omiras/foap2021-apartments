719de04836b4d6c2139b2b8c2c66ed2b23f4b595
oskytar <oscar.miras@gmail.com>
2021-06-16 18:11:29 +0200

    endpoint recibe la modificacion de apartamento

--- routes/AdminApartmentRoutes.js
+++ routes/AdminApartmentRoutes.js
@@ -5,6 +5,19 @@ const Apartment = require('../models/ApartmentModel');
 
 // Vamos a registrar todas las rutas que tienen que ver con la manipulación de los apartamentos por parte de un usuario de tipo 'Administrador'
 
+router.get('/apartment/:idApartment', async (req, res) => {
+    const { idApartment } = req.params;
+    // const idApartment = req.params.idApartment
+
+    // Ir a la base de datos para recuperar el apartamento con el id idApartment
+    const apartment = await Apartment.findById(idApartment);
+    
+    // Vamos a renderizar el formulario con los valores precargados
+    res.render('add-apartment', {
+        apartment: apartment
+    })
+})
+
 router.get('/', async (req, res)=> {
     const allApartments = await Apartment.find();
     res.render('index', {
@@ -14,10 +27,26 @@ router.get('/', async (req, res)=> {
 })
 
 router.get('/add-new', (req, res) => {
-    res.render('add-apartment');
+    res.render('add-apartment', {
+        apartment: {}
+    });
 })
 
 router.post('/add-new', async (req, res) => {
+
+    // Comprobar si recibimos un idApartment significa que queremos modificar el apartmento. En caso contrario, insertar un documento nuevo.
+
+    const idApartment = req.body.idApartment;
+    if (idApartment) {
+        // Realizar un updateOne !!
+        console.log('Queremos modificar el apartmento! Usa Mongoose para hacerlo!')
+        return res.send('Modificar apartamento!');
+    }
+
+    else {
+        console.log('Es un apartamento nuevo!')
+    }
+
     const title = req.body.title;
     const price = req.body.price;
     const size = req.body.size;
--- routes/UserApartmentRoutes.js
+++ routes/UserApartmentRoutes.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 const Apartment = require('../models/ApartmentModel');
 
+router.get('/apartment/:idApartment', async (req, res) => {
+    const { idApartment } = req.params;
+    // const idApartment = req.params.idApartment
+
+    // Ir a la base de datos para recuperar el apartamento con el id idApartment
+    const apartment = await Apartment.findById(idApartment);
+
+    res.render('detail.ejs', {
+        apartment: apartment
+    });
+})
+
 // Definimos las rutas de los clientes "normales" (los que quieren alquilar los apartamentos)
 
 router.get('/', async (req, res) => {
