4ef385c1d667c722daab2e5e8adac70ff36f53d1
Oscar <oscar.miras@gmail.com>
2021-07-12 09:05:16 +0200

    e2e tests

--- test/apartments.test.js
+++ test/apartments.test.js
@@ -1,10 +1,9 @@
-const {jest} = require('@jest/globals'); 
 
 // -- e2e tests --
 test('crear apartamento. Puedo crear un apartamento y luego listarlo en la lista de apartamentos', ()=>{
-
+    
 })
 
-test('Buscar apartamentos. Dado un precio "random"; entre 0 y 100; realizo una búsqueda. No deben listarse apartamentos con un precio mayor al valor random', ()=>{
+test('Buscar apartamentos. Dado un precio muy bajo (1 €); debe ocurrir que si realizamos una búsqueda con dicho valor, NO debemos obtener ningun apartamento en la lista de apartamentos. ', ()=>{
     
 })
