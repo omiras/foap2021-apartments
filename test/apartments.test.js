63f405a250e6a7e5170ab6a0e5a082947d82ea92
Oscar <oscar.miras@gmail.com>
2021-07-12 08:43:12 +0200

    e2e test strcuture

---
+++ test/apartments.test.js
@@ -0,0 +1,10 @@
+const {jest} = require('@jest/globals'); 
+
+// -- e2e tests --
+test('crear apartamento. Puedo crear un apartamento y luego listarlo en la lista de apartamentos', ()=>{
+
+})
+
+test('Buscar apartamentos. Dado un precio "random"; entre 0 y 100; realizo una búsqueda. No deben listarse apartamentos con un precio mayor al valor random', ()=>{
+    
+})
